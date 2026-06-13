5021a9522ce901a08fe4c5c34e37d5b6910762b1
secondio10 <168173824+secondioprawiro@users.noreply.github.com>
2026-06-13 12:32:20 +0700

    feat(sdk): export public API from index barrel

--- packages/batutas-sdk/src/index.ts
+++ packages/batutas-sdk/src/index.ts
@@ -1,1 +1,5 @@
-export {};
+export { BATUTAS_ADDRESS, BATUTAS_CHAIN_ID, CELO_EXPLORER, PEG } from "./contract";
+export type { Move, Outcome } from "./moves";
+export { MOVE_NUM, NUM_TO_MOVE, NUM_TO_RESULT } from "./moves";
+export { makeSecret, buildCommitHash } from "./commit";
+export { batutasAbi } from "./abi";
